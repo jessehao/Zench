db44931f0450b9456d7e0cb00ce4c071469b8e3b
Jesse Hao <jeasygates@hotmail.com>
2018-10-19 19:59:27 +0800

    Initial Commit

---
+++ Zench/Zench.h
@@ -0,0 +1,19 @@
+//
+//  Zench.h
+//  Zench
+//
+//  Created by Jesse Hao on 2018/10/19.
+//  Copyright © 2018 Snoware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Zench.
+FOUNDATION_EXPORT double ZenchVersionNumber;
+
+//! Project version string for Zench.
+FOUNDATION_EXPORT const unsigned char ZenchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Zench/PublicHeader.h>
+
+
