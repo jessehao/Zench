7e49e3928f4e5b138fef5ee2fc84525e93d91d46
Jesse Hao <jeasygates@hotmail.com>
2018-10-24 17:21:52 +0800

    add: BaseView

---
+++ Zench/View/BaseView.swift
@@ -0,0 +1,53 @@
+//
+//  BaseView.swift
+//  Zench
+//
+//  Created by Jesse Hao on 2018/10/24.
+//  Copyright © 2018 Snoware. All rights reserved.
+//
+
+import UIKit
+
+class BaseView: UIView {
+	var defaultBackgroundColor:UIColor { return .white }
+	
+	// MARK: - Lifecycle
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		self.setup()
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		self.setup()
+	}
+	
+	// MARK: - Operations
+	func setup() {
+		self.backgroundColor = self.defaultBackgroundColor
+		self.prepareSubviews()
+		self.makeConstraints()
+		self.prepareTargets()
+	}
+	func prepareSubviews() {}
+	func makeConstraints() {}
+	func prepareTargets() {}
+}
+
+
+class StandardShapeView: BaseView {
+	override class var layerClass : AnyClass { return CAShapeLayer.self }
+	var shapeLayer:CAShapeLayer { return self.layer as! CAShapeLayer }
+	
+	func setDashedBorder(withThickness thickness:CGFloat,
+						 borderColor:UIColor,
+						 dotWidth:NSNumber = 2,
+						 separatorWidth:NSNumber = 2) {
+		self.shapeLayer.fillColor = UIColor.clear.cgColor
+		self.shapeLayer.strokeColor = borderColor.cgColor
+		self.shapeLayer.lineWidth = thickness
+		self.shapeLayer.lineJoin = CAShapeLayerLineJoin.round
+		self.shapeLayer.lineDashPhase = 0
+		self.shapeLayer.lineDashPattern = [dotWidth, separatorWidth]
+	}
+}
