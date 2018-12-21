b8cd9de19f33e227e5e0493dbabed72054a1b2ba
Jesse Hao <jeasygates@hotmail.com>
2018-12-21 14:04:52 +0800

    update: StandardTextView + attributedText

--- Zench/View/Components/StandardTextView.swift
+++ Zench/View/Components/StandardTextView.swift
@@ -61,6 +61,20 @@ open class StandardTextView : BaseTextView {
 		}
 	}
 	
+	open override var attributedText: NSAttributedString? {
+		get {
+			return self.isPlaceholderHidden ? super.attributedText : nil
+		}
+		set {
+			if newValue == nil || newValue?.string.isEmpty == true {
+				self.showPlaceholderInContentTextField()
+				return
+			}
+			self.hidePlaceholderInContentTextField()
+			super.attributedText = newValue
+		}
+	}
+	
 	// MARK: - Events
 	@objc open func textViewTextDidBeginEditingReceived(notification:Notification) {
 		guard self == notification.object as? StandardTextView else { return }
@@ -93,14 +107,20 @@ open class StandardTextView : BaseTextView {
 	}
 	
 	open func showPlaceholderInContentTextField() {
-		super.text = self.placeholder
-		super.textColor = self.placeholderTextColor
+		var attributes:[NSAttributedString.Key : Any] = [:]
+		if let font = self.font {
+			attributes[.font] = font
+		}
+		if let color = self.placeholderTextColor {
+			attributes[.foregroundColor] = color
+		}
+		super.attributedText = self.placeholder.attributedString(withAttributes: attributes)
 		self.isPlaceholderHidden = false
 	}
 	
 	open func hidePlaceholderInContentTextField() {
 		super.textColor = self.foregroundTextColor
-		super.text = ""
+		super.attributedText = nil
 		self.isPlaceholderHidden = true
 	}
 }
