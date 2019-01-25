331fd4c119ca27c3c03dca33a20bef687e1af706
Jesse Hao <jeasygates@hotmail.com>
2019-01-25 11:04:46 +0800

    update: + Form / Form View Controller

---
+++ Zench/Form/StandardFormViewController.swift
@@ -0,0 +1,213 @@
+//
+//  StandardFormViewController.swift
+//  Zench
+//
+//  Created by Jesse Hao on 2019/1/25.
+//  Copyright © 2019 Snoware. All rights reserved.
+//
+
+class StandardFormController<FormType:StandardForm>: GeneralTableViewController {
+	override var style: UITableView.Style { return .grouped }
+	
+	let headerIdentifier = "HeaderIdentifier"
+	
+	let form:FormType = FormType()
+	
+	var selectedIndexPath:IndexPath?
+	var editingIndexPath:IndexPath?
+	
+	// MARK: - Lifecycle
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.configForm(self.form)
+		self.registerDynamicRows()
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		self.clearsSelectionIfNeeded()
+	}
+	
+	// MARK: - Form Events
+	func configForm(_ form:FormType) {
+		form.delegate = self
+	}
+	func configDynamicCell(_ cell:UITableViewCell, withIdentifier identifier:String, at index:Int) {}
+	func numberOfRows(forDynamicCellReuseIdentifier identifier:String) -> Int { return 0 }
+	func didSelectDynamicRow(forIdentifier identifier:String, at index:Int) {}
+	func willDeleteDynamicCell(forIdentifier identifier:String, at index:Int) {}
+	
+	// MARK: - UITableView Data Source
+	override func numberOfSections(in tableView: UITableView) -> Int {
+		return self.form.count
+	}
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		let sec = self.form[section]
+		let retval = sec.reduce(0) { sum, row in
+			row.dynamicRowCount = row.isDynamic ? self.numberOfRows(forDynamicCellReuseIdentifier: row.reuseIdentifier!) : 1
+			return sum + row.dynamicRowCount
+		}
+		return retval
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let rowWithOffset = self.form[indexPath]
+		if rowWithOffset.row.isDynamic, let identifier = rowWithOffset.row.reuseIdentifier {
+			let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+			self.configDynamicCell(cell, withIdentifier: identifier, at: rowWithOffset.offset)
+			return cell
+		}
+		return rowWithOffset.row.cell!
+	}
+	
+	// MARK: - UITableView Delegate
+	override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+		return nil
+	}
+	
+	override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+		return self.form[section].header ?? " "
+	}
+	
+	override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+		return nil
+	}
+	
+	override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+		let rowWithOffset = self.form[indexPath]
+		if rowWithOffset.row.canSelect {
+			return indexPath
+		}
+		return nil
+	}
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		super.tableView(tableView, didSelectRowAt: indexPath)
+		let rowWithOffset = self.form[indexPath]
+		if rowWithOffset.row.isDynamic {
+			self.didSelectDynamicRow(forIdentifier: rowWithOffset.row.reuseIdentifier!, at: rowWithOffset.offset)
+		} else {
+			rowWithOffset.row.didSelectRowEventHandler?()
+		}
+	}
+	
+	override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+		return UITableView.automaticDimension
+	}
+	
+	override func tableView(_ tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat {
+		return 10
+	}
+	
+	override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+		return self.form[section].footer != nil ? UITableView.automaticDimension : CGFloat.leastNormalMagnitude
+	}
+	
+	override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+		return self.form[section].footer ?? " "
+	}
+	
+	override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+		return self.form[indexPath].row.canDelete
+	}
+	
+	override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+		if self.form[indexPath].row.canDelete { return .delete }
+		return .none
+	}
+	
+	override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+		let rowWithOffset = self.form[indexPath]
+		if rowWithOffset.row.canDelete {
+			if rowWithOffset.row.isDynamic {
+				self.willDeleteDynamicCell(forIdentifier: rowWithOffset.row.reuseIdentifier!, at: rowWithOffset.offset)
+				tableView.deleteRows(at: [indexPath], with: .top)
+			} else {
+				let formRowIndex = self.form[indexPath.section].formRowIndexWithRelativeOffset(forTableViewRowIndex: indexPath.row).index
+				tableView.deleteRows(at: [[indexPath.section, formRowIndex]], with: .top)
+			}
+		}
+	}
+	
+	// MARK: - Actions
+	override func keyboardWillShow(userInfo: StandardKeyboardNotificationUserInfo) {
+		let keyboardRect = userInfo.beginFrame
+		let keyboardSize = keyboardRect?.size ?? .zero
+		var contentInsets:UIEdgeInsets
+		let offset:CGFloat = 40
+		if UIApplication.shared.statusBarOrientation.isPortrait {
+			contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height + offset, right: 0)
+		} else {
+			contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.width + offset, right: 0)
+		}
+		let rate = userInfo.animationDuration ?? 0
+		UIView.animate(withDuration: rate) {
+			self.tableView.contentInset = contentInsets
+			self.tableView.scrollIndicatorInsets = contentInsets
+		}
+		if let indexPath = self.editingIndexPath {
+			self.tableView.scrollToRow(at: indexPath, at: UITableView.ScrollPosition.top, animated: true)
+			self.editingIndexPath = nil
+		}
+	}
+	
+	override func keyboardWillHide(userInfo: StandardKeyboardNotificationUserInfo) {
+		let rate = userInfo.animationDuration ?? 0
+		UIView.animate(withDuration: rate) {
+			self.tableView.contentInset = .zero
+			self.tableView.scrollIndicatorInsets = .zero
+		}
+	}
+	
+	// MARK: - Operations
+	override func configTableView(_ tableView: StandardTableView) {
+		super.configTableView(tableView)
+		tableView.keyboardDismissMode = .onDrag
+	}
+	
+	func registerDynamicRows() {
+		self.form.allDynamicRows().forEach {
+			guard let identifier = $0.reuseIdentifier else { return }
+			self.tableView.register($0.cellType, forCellReuseIdentifier: identifier)
+		}
+	}
+}
+
+extension StandardFormController : StandardFormDelegate {
+	func form(_ form: StandardForm, sectionsAddedAt indexes: IndexSet) {
+		self.tableView.beginUpdates()
+		self.tableView.insertSections(indexes, with: .fade)
+		self.tableView.endUpdates()
+	}
+	
+	func form(_ form: StandardForm, sectionsRemovedAt indexes: IndexSet) {
+		self.tableView.beginUpdates()
+		self.tableView.deleteSections(indexes, with: .fade)
+		self.tableView.endUpdates()
+	}
+	
+	func form(_ form: StandardForm, sectionsUpdatedAt indexes: IndexSet) {
+		self.tableView.beginUpdates()
+		self.tableView.reloadSections(indexes, with: .automatic)
+		self.tableView.endUpdates()
+	}
+	
+	func form(_ form: StandardForm, rowsAddedAt indexPaths: [IndexPath]) {
+		self.tableView.beginUpdates()
+		self.tableView.insertRows(at: indexPaths, with: .middle)
+		self.tableView.endUpdates()
+	}
+	
+	func form(_ form: StandardForm, rowsRemovedAt indexPaths: [IndexPath]) {
+		self.tableView.beginUpdates()
+		self.tableView.deleteRows(at: indexPaths, with: .top)
+		self.tableView.endUpdates()
+	}
+	
+	func form(_ form: StandardForm, rowsUpdatedAt indexPaths: [IndexPath]) {
+		self.tableView.beginUpdates()
+		self.tableView.reloadRows(at: indexPaths, with: .automatic)
+		self.tableView.endUpdates()
+	}
+}
