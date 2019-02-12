1bb2417ff3fa6ecffac6e69abf2efca90d95cf23
Jesse Hao <jeasygates@hotmail.com>
2019-02-12 18:27:18 +0800

    update: CollectionExtensions

--- Zench/Extensions/CollectionExtensions.swift
+++ Zench/Extensions/CollectionExtensions.swift
@@ -6,6 +6,13 @@
 //  Copyright © 2018 Snoware. All rights reserved.
 //
 
+public extension Collection {
+	func element(at index:Self.Index) -> Element? {
+		guard (self.startIndex..<self.endIndex).contains(index) else { return nil }
+		return self[index]
+	}
+}
+
 public extension Collection where Element == Bool {
 	func and() -> Bool {
 		guard self.count > 0 else { return false }
@@ -20,6 +27,43 @@ public extension Collection where Element == Bool {
 			if i == true { return true }
 		}
 		return false
+	}
+}
+
+public extension RangeReplaceableCollection {
+	mutating func replaceAll(with value:Element) {
+		self = Self(repeating: value, count: self.count)
+	}
+	
+	@discardableResult
+	mutating func appendIfNonnull(_ newElement: Element?) -> Bool {
+		if let new = newElement {
+			self.append(new)
+			return true
+		}
+		return false
+	}
+}
+
+public extension RangeReplaceableCollection where Element : Equatable {
+	@discardableResult
+	mutating func remove(_ element:Element) -> Bool {
+		guard let index = self.index(of: element) else { return false }
+		self.remove(at: index)
+		return true
+	}
+	
+	@discardableResult
+	mutating func appendUnique(_ newElement:Element) -> Bool {
+		guard !(self.contains { $0 == newElement }) else { return false }
+		self.append(newElement)
+		return true
+	}
+}
+
+public extension Sequence {
+	func array() -> [Element] {
+		return Array(self)
 	}
 }
 
