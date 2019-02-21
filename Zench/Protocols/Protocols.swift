f795398ac6438044825faef372c40cd166104605
Jesse Hao <jeasygates@hotmail.com>
2019-02-21 18:43:28 +0800

    update: + Support + ConvenientRightNavigationBarButtonItemSupport

--- Zench/Protocols/Protocols.swift
+++ Zench/Protocols/Protocols.swift
@@ -15,24 +15,6 @@ public protocol StandardNullable {
 	var orNil:Self? { get }
 }
 
-// MARK: - Standard Collection Support
-public protocol StandardCollectionSupport : StandardNullable {}
-extension Array : StandardCollectionSupport {}
-extension Dictionary : StandardCollectionSupport {}
-extension Set : StandardCollectionSupport {}
-
-extension String : StandardNullable {
-	public var orNil: String? {
-		return self.isEmpty ? nil : self
-	}
-}
-
-public extension Collection where Self : StandardCollectionSupport {
-	var orNil:Self? {
-		return self.isEmpty ? nil : self
-	}
-}
-
 // MARK: - Copying
 public protocol StandardDuplicatable {
     func duplicate() throws -> Self
@@ -40,43 +22,6 @@ public protocol StandardDuplicatable {
 
 public extension StandardDuplicatable where Self : Codable {
 	public func duplicate() throws -> Self { return try JSONDecoder().decode(Self.self, from: try JSONEncoder().encode(self)) }
-}
-
-// MARK: - Notification Support
-public protocol StandardNotificationSupport {
-	/// add specific selector to observe a specific notification name of notification center.
-	///
-	/// - Parameters:
-	///   - selector: the target selector when the notification received
-	///   - notificationName: the notification name that you wanna observe
-	func addSelector(_ selector:Selector, onNotificationName notificationName:Notification.Name)
-	/// Listen to a specific notification name and handles event when received
-	///
-	/// - Parameters:
-	///   - notificationName: the notification name that you wanna listen to
-	///   - handler: a handler called when notification received
-	func listenToNotificationName(_ notificationName:Notification.Name, using handler:@escaping (Notification) -> Void)
-}
-
-public extension StandardNotificationSupport {
-	func addSelector(_ selector:Selector, onNotificationName notificationName:Notification.Name) {
-		NotificationCenter.default.addObserver(self, selector: selector, name: notificationName, object: nil)
-	}
-	
-	func listenToNotificationName(_ notificationName:Notification.Name, using handler:@escaping (Notification) -> Void) {
-		NotificationCenter.default.addObserver(forName: notificationName, object: nil, queue: nil, using: handler)
-	}
-}
-
-// MARK: - Pasteboard Support
-public protocol StandardPasteboardSupport {
-	func copyToGeneralPasteboard(withString string:String)
-}
-
-public extension StandardPasteboardSupport {
-	func copyToGeneralPasteboard(withString string:String) {
-		UIPasteboard.general.string = string
-	}
 }
 
 // MARK: - Localizable
---
+++ Zench/Protocols/Supports.swift
@@ -0,0 +1,86 @@
+//
+//  Supports.swift
+//  Zench
+//
+//  Created by Jesse Hao on 2019/2/21.
+//  Copyright © 2019 Snoware. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Standard Collection Support
+public protocol StandardCollectionSupport : StandardNullable {}
+extension Array : StandardCollectionSupport {}
+extension Dictionary : StandardCollectionSupport {}
+extension Set : StandardCollectionSupport {}
+
+extension String : StandardNullable {
+	public var orNil: String? {
+		return self.isEmpty ? nil : self
+	}
+}
+
+public extension Collection where Self : StandardCollectionSupport {
+	var orNil:Self? {
+		return self.isEmpty ? nil : self
+	}
+}
+
+// MARK: - Notification Support
+public protocol StandardNotificationSupport {
+	/// add specific selector to observe a specific notification name of notification center.
+	///
+	/// - Parameters:
+	///   - selector: the target selector when the notification received
+	///   - notificationName: the notification name that you wanna observe
+	func addSelector(_ selector:Selector, onNotificationName notificationName:Notification.Name)
+	/// Listen to a specific notification name and handles event when received
+	///
+	/// - Parameters:
+	///   - notificationName: the notification name that you wanna listen to
+	///   - handler: a handler called when notification received
+	func listenToNotificationName(_ notificationName:Notification.Name, using handler:@escaping (Notification) -> Void)
+}
+
+public extension StandardNotificationSupport {
+	func addSelector(_ selector:Selector, onNotificationName notificationName:Notification.Name) {
+		NotificationCenter.default.addObserver(self, selector: selector, name: notificationName, object: nil)
+	}
+	
+	func listenToNotificationName(_ notificationName:Notification.Name, using handler:@escaping (Notification) -> Void) {
+		NotificationCenter.default.addObserver(forName: notificationName, object: nil, queue: nil, using: handler)
+	}
+}
+
+// MARK: - Pasteboard Support
+public protocol StandardPasteboardSupport {
+	func copyToGeneralPasteboard(withString string:String)
+}
+
+public extension StandardPasteboardSupport {
+	func copyToGeneralPasteboard(withString string:String) {
+		UIPasteboard.general.string = string
+	}
+}
+
+// MARK: - ConvenientRightNavigationBarButtonItemSupport
+/// Conform this protocol could help you set right navigation bar button item really fast.
+///
+///	all you need to do is:
+/// 1. write `func rightNavigationBarButtonItemTouched(sender:UIBarButtonItem)` in your adopter body.
+///	2. if adopted by a UIViewController or its subclass, invoke `setRightNavigationBarButtonItem(_ onConfig:(UIBarButtonItem) -> Void)` after the navigation controller loaded the destination view controller.
+@objc
+protocol ConvenientRightNavigationBarButtonItemSupport : class {
+	@objc
+	func rightNavigationBarButtonItemTouched(sender:UIBarButtonItem)
+	
+	var navigationItem: UINavigationItem { get }
+}
+
+extension ConvenientRightNavigationBarButtonItemSupport {
+	func setRightNavigationBarButtonItem(_ onConfig:(UIBarButtonItem) -> Void) {
+		let item = UIBarButtonItem(title: nil, style: .plain, target: self, action: #selector(self.rightNavigationBarButtonItemTouched(sender:)))
+		onConfig(item)
+		self.navigationItem.rightBarButtonItem = item
+	}
+}
